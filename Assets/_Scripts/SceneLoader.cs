4d871ffb8e5e6f49d53a968d749682996510cec2
Gwan Yip <gwan@codeandcraft.nyc>
2017-09-20 15:14:23 -0400

    Initial commit

---
+++ Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public float delay = 10f;
+    public string m_LoadLevelName;
+
+    private void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "01_SplashScreen") {
+            Debug.Log("Loading next scene after " + delay);
+            StartCoroutine(LoadLevelAfterDelay(delay));
+        }
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay) {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(m_LoadLevelName, LoadSceneMode.Single);
+    }
+
+    public void SceneSelection() {
+        Debug.Log("Loading next scene " + m_LoadLevelName);
+        SceneManager.LoadScene(m_LoadLevelName, LoadSceneMode.Single);
+    }
+}
